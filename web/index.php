263f4f8c1fbc5d3a2a3942cf58e93b09e1dc587a
sbellalarm <54110321+sbellalarm@users.noreply.github.com>
2021-02-21 00:46:01 -0500

    staging

--- web/index.php
+++ web/index.php
@@ -16,7 +16,7 @@ if (class_exists('Dotenv\Dotenv') && file_exists(CRAFT_BASE_PATH.'/.env')) {
 }
 
 // Load and run Craft
-define('CRAFT_ENVIRONMENT', getenv('ENVIRONMENT') ?: 'production');
+define('CRAFT_ENVIRONMENT', getenv('ENVIRONMENT') ?: 'staging');
 /** @var craft\web\Application $app */
 $app = require CRAFT_VENDOR_PATH.'/craftcms/cms/bootstrap/web.php';
 $app->run();
